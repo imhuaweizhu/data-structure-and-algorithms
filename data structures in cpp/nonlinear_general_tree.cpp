3ca944ec304c9860af675e0b6e6eb2d1c3e9636a
Huawei Zhu <huawei.acarer@gmail.com>
2021-04-07 00:20:11 -0400

    Update and rename nonlinear_generic_tree.cpp to nonlinear_general_tree.cpp

--- data structures in cpp/nonlinear_general_tree.cpp
+++ data structures in cpp/nonlinear_general_tree.cpp
@@ -1,4 +1,9 @@
-/* A tree has following general properties:
+/* 
+Reference: http://www.cs.cmu.edu/~clo/www/CMU/DataStructures/Lessons/lesson4_1.htm
+CMU course 15-111: Intermediate and Advanced Programming (2005 - 2008) by Prof. Ananda Gunawardena
+
+A tree has following general properties:
+
 1. One node is distinguished as a root;
 2. Every node (exclude a root) is connected by a directed edge from exactly one other node; A direction is: parent -> children
 3. Each node can have arbitrary number of children.
@@ -14,9 +19,9 @@ Depth of a tree: the number of edges from the root to the deepest leaf.
 Height of a node: the number of edges from the node to the deepest leaf. As you can see, this definition rely on the definition of depth of a node.
 Height of a tree: the number of edges from the root to the deepest leaf.
 
-Reference: http://www.cs.cmu.edu/~clo/www/CMU/DataStructures/Lessons/lesson4_1.htm
-CMU course 15-111: Intermediate and Advanced Programming (2005 - 2008) by Prof. Ananda Gunawardena
+
 */
+
 
 
 
